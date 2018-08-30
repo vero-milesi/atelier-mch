5662c9c2e55f0e6f5fbffe3b72b0d367922ca13b
vero-milesi <verodelagnes@gmail.com>
2018-08-30 14:20:45 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.photo img').mouseover(function(){
+        $('.on').slideDown(1000)
+    
+    })
+    $('.photo img').mouseleave(function(){
+        $('.cacher').slideUp(1000)
+    })
+    
+     })
